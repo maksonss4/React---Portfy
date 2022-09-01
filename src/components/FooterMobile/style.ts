eecec75d5471f2091b6edcafd98b99c64d973a9c
= <r.netofreire45@gmail.com>
2022-09-01 20:17:53 -0300

    feat/FooterFix

--- src/components/FooterMobile/style.ts
+++ src/components/FooterMobile/style.ts
@@ -2,10 +2,23 @@ import styled from "styled-components";
 
 export const ContainerFooterMobile = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  width: 100vw;
+  width: 100%;
   padding: 10px;
   background: linear-gradient(91.66deg, #47dac5 9.15%, #4a90e2 90.02%);
   color: var(--white);
+  position: fixed;
+  .FooterIcons {
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    max-width: 400px;
+  }
+  @media (min-width: 1000px) {
+    & {
+      display: none;
+    }
+  }
 `;
