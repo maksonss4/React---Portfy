cbca319f61fadf2e10542008f826e42278a760fd
= <r.netofreire45@gmail.com>
2022-09-01 13:34:48 -0300

    feat/FinishedHeader/ResponsiveHeader

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Header } from "./components/Header";
 import { Global } from "./styles";
 
 function App() {
   return (
     <div className="App">
       <Global />
+      <Header />
     </div>
   );
 }
