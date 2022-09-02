683adc3e29c0dd25b4e7443cad19f35b33347884
Mazuquiebe Filho <mazuquiebe75@gmail.com>
2022-09-02 11:05:02 -0300

    feat:PostList

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Global } from "./styles";
 import { LandingPage } from "./pages/LandingPage";
-
+import { postList1 } from "./components/PostList/postTestes";
+import PostList from "./components/PostList";
 const App = () => {
   return (
     <div className="App">
       <Global />
-      <LandingPage />
+      {/* <LandingPage />*/}
+      {/*<PostList postList={postList1} />*/}
     </div>
   );
-}
+};
 
 export default App;
