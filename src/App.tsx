ac07d5076aa317366a60c6f553d8dc58b1facc91
Mazuquiebe Filho <mazuquiebe75@gmail.com>
2022-09-05 10:21:41 -0300

    fix:responsivity-post

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import SwitchProvider from "./contexts/SwitchContext";
 import AppRoutes from "./routes/routes";
 import { Global } from "./styles";
-import Feed from "./pages/Feed";
 
 const App = () => {
   return (
     <div className="App">
       <Global />
       <SwitchProvider>
-        {/*<AppRoutes />*/}
-        <Feed />
+        <AppRoutes />
       </SwitchProvider>
     </div>
   );
